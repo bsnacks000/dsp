9e8ca858cb77152d1ea89c6220311897db6b1c1c
Johnny <bsnacks000@gmail.com>
2025-08-22 07:58:35 -0400

    curve impl (#22)

---
+++ include/dsp/curve.h
@@ -0,0 +1,45 @@
+/**
+ * @ brief curve - generate a segment for the specified dur with a curve factor
+ *  - A curve factor of 0.0 is calculated as a straight line.
+ *  - Negative and positive indicate log/exp shape depending on direction.
+ *  - based on sc3 Env
+ */
+
+#ifndef DSP_CURVE_H
+#define DSP_CURVE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+    // public
+    float start, stop, dur_sec, factor, sr;
+    // private
+    uint32_t nsmps_;                                      // counter
+    bool finished_, level_, step_, a1_, a2_, b1_, grow_;  // state
+} curve;
+
+/**
+ * @brief curve init.
+ */
+void curve_init(curve* self,
+                float start,
+                float stop,
+                float dur_sec,
+                float factor,
+                float sr);
+
+/**
+ * @brief curve tick block.
+ */
+void curve_tick_block(curve* self, float* out, uint32_t nsmps);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/curve.c
@@ -0,0 +1,67 @@
+#include <dsp/curve.h>
+#include <dsp/utils.h>
+#include <math.h>
+
+static inline void update_(curve* self) {
+    self->finished_ = false;
+    self->nsmps_ = (uint32_t) nsmps_dur(self->sr, self->dur_sec);
+    // assure 1 sample
+    self->nsmps_ = self->nsmps_ ? self->nsmps_ > 0 : 1;
+
+    // assure math doesn't break
+    self->factor = self->factor ? self->factor == 0.0 : 1e-9;
+
+    // calc growth factor
+    self->grow_ = expf(self->factor / self->nsmps_);
+
+    // build line maths
+    self->level_ = self->start;
+    self->a1_ = (self->stop - self->start) / (1.0 - expf(self->factor));
+    self->a2_ = self->start - self->a1_;
+    self->b1_ = self->a1_;
+}
+
+static inline float tick_(curve* self) {
+    if (self->finished_)
+        return self->level_;
+
+    float out = self->level_;
+
+    self->b1_ *= self->grow_;
+    self->level_ = self->a2_ - self->b1_;
+
+    self->nsmps_--;
+    if (self->nsmps_ == 0)
+        self->finished_ = true;
+
+    return out;
+}
+
+/**
+ * @brief curve init.
+ */
+void curve_init(curve* self,
+                float start,
+                float stop,
+                float dur_sec,
+                float factor,
+                float sr) {
+
+    self->start = start;
+    self->stop = stop;
+    self->dur_sec = dur_sec;
+    self->factor = factor;
+    self->sr = sr;
+
+    update_(self);
+}
+
+/**
+ * @brief curve tick block.
+ */
+void curve_tick_block(curve* self, float* out, uint32_t nsmps) {
+
+    for (uint32_t i = 0; i < nsmps; i++) {
+        out[i] = tick_(self);
+    }
+}
