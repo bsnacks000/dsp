a629cbf961556f8aed8e931602991c49c426cddd
bsnacks000 <bsnacks000@gmail.com>
2025-08-31 12:18:38 -0400

    line boiler

---
+++ csnd/line.c
@@ -0,0 +1,9 @@
+#include "line.h"
+
+int lline_init(CSOUND* csound, lline* obj) {
+    return OK;
+}
+
+int lline_vector(CSOUND* csound, lline* obj) {
+    return OK;
+}
---
+++ csnd/line.h
@@ -0,0 +1,25 @@
+#ifndef DSP_CSND_LINE_H
+#define DSP_CSND_LINE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <csdl.h>
+#include <dsp/line.h>
+
+typedef struct {
+    OPDS h;
+    MYFLT *a_out, *i_start, *i_end;
+    line l;
+} lline;
+
+int lline_init(CSOUND* csound, lline* obj);
+
+int lline_vector(CSOUND* csound, lline* obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
