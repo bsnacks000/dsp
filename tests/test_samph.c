0e1f9ae7fc98e230cf86ea9d4bd5372211118218
bsnacks000 <bsnacks000@gmail.com>
2025-12-20 17:17:40 -0500

    add samph coverage

---
+++ tests/test_samph.c
@@ -0,0 +1,44 @@
+#include "test_samph.h"
+#include <dsp/samph.h>
+
+#include <stdio.h>
+#include "helpers.h"
+
+#define NSMPS 64
+
+MunitResult test_samph_init(const MunitParameter params[], void* data) {
+    (void) params;
+    (void) data;
+
+    samph sh;
+    samph_init(&sh, 0.5);
+
+    return MUNIT_OK;
+}
+
+MunitResult test_samph_tick_block(const MunitParameter params[], void* data) {
+    (void) params;
+    (void) data;
+
+    samph sh;
+    samph_init(&sh, 0.5);
+
+    float out[NSMPS] = {0.0};
+    float in[NSMPS] = {0.0};
+    float gate[NSMPS] = {0.0};
+    float gate_thresh[NSMPS] = {0.0};
+
+    fill_random_bipolar_signal(in, NSMPS);
+    fill_dc(gate_thresh, 0.5, NSMPS);
+
+    // trigger gate half way through cycle
+    for (int i = NSMPS / 2; i < NSMPS; i++)
+        gate[i] = 1.0;
+
+    samph_tick_block(&sh, out, in, gate, gate_thresh, NSMPS);
+
+    for (int i = 0; i < NSMPS; i++)
+        printf("%.3f\n", out[i]);
+
+    return MUNIT_OK;
+}
